9bb5144363aff38cff941a278e9e4b9ac0749d8c
mikel evins <mevins@me.com>
2024-07-21 11:39:20 -0500

    export more symbols

--- package.lisp
+++ package.lisp
@@ -4,5 +4,18 @@
   (:use #:cl)
   (:nicknames :ksuid)
   (:export
+   #:+128-set-bits+
+   #:+base62-alphabet+
+   #:+ksuid-universal-time-epoch+
+   #:+ksuid-unix-epoch-offset+
+   #:+unix-universal-time-epoch+
+   #:integer->ksuid
    #:ksuid
-   #:make-ksuid))
+   #:ksuid->integer
+   #:ksuid->string
+   #:make-ksuid
+   #:string->ksuid
+   #:string->ksuid-int
+   #:valid-ksuid-string-p
+   ))
+
